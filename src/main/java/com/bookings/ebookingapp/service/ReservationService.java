981b771ccdfcb249fdfe6de824aa070becb3bca0
Robert Pietrzykowski <rpietrzykowski@me.com>
2025-02-23 21:44:20 +0100

    - Fix the annotations for the validation - Fix the swagger configuration

--- src/main/java/com/bookings/ebookingapp/service/ReservationService.java
+++ src/main/java/com/bookings/ebookingapp/service/ReservationService.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Service
