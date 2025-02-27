0d927983ad647c729599a37e2642d648e13d055e
Robert Pietrzykowski <32705460+Rpietrzykowski@users.noreply.github.com>
2025-02-27 20:07:59 +0100

    Merge pull request #3
    * Add the proper steps

--- src/test/java/com/bookings/ebookingapp/steps/BookingSteps.java
+++ src/test/java/com/bookings/ebookingapp/steps/BookingSteps.java
@@ -2,40 +2,63 @@ package com.bookings.ebookingapp.steps;
 
 import com.bookings.ebookingapp.model.BookingRequest;
 import com.bookings.ebookingapp.repository.BookingRequestRepository;
+import com.bookings.ebookingapp.service.ReservationService;
+import com.bookings.ebookingapp.utils.BookingKafkaListener;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+import org.springframework.test.context.event.annotation.AfterTestExecution;
+import org.springframework.test.context.event.annotation.AfterTestMethod;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ContextConfiguration
 public class BookingSteps {
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    ReservationService reservationService;
 
     @Autowired
     private BookingRequestRepository bookingRequestRepository;
 
-    private String bookingRequestJson;
+    private final BookingRequest bookingRequest = new BookingRequest();
 
-    @Given("a booking request JSON")
-    public void a_booking_request_json() {
-        bookingRequestJson = "{ \"bookingnumber\":222222, \"name\": \"John\", \"items\": 3 }";
+    private final BookingKafkaListener bookingKafkaListener = new BookingKafkaListener();
+
+    private KafkaConsumer<String, BookingRequest> kafkaConsumer;
+
+    @Given("^a booking request JSON with booking number (.*) name (.*) and items (.*)$")
+    public void aBookingRequestJSONWithBookingNumberBookingNumberNameNameAndItemsItems(Integer bookingNumber, String name, Integer items) {
+        bookingRequest.setBookingNumber(bookingNumber);
+        bookingRequest.setName(name);
+        bookingRequest.setItems(items);
     }
 
-    @When("the booking request is consumed")
-    public void the_booking_request_is_consumed() {
-        kafkaTemplate.send("TEST.BOOKINGREQUEST.TOPIC", bookingRequestJson);
+    @When("^the booking request is send$")
+    public void the_booking_request_is_send() {
+        reservationService.createBooking(bookingRequest);
     }
 
-    @Then("the booking request should be saved in MongoDB")
-    public void the_booking_request_should_be_saved_in_mongodb() {
-        // Add assertions to verify the booking request is saved in MongoDB
-        BookingRequest bookingRequest = bookingRequestRepository.findById(22222).orElse(null);
-        assert bookingRequest != null;
-        assert bookingRequest.getName().equals("John");
-        assert bookingRequest.getItems() == 3;
+    @Then("^the booking reservation (.*) should be saved in MongoDB with name (.*) and items (.*)$")
+    public void the_booking_request_should_be_saved_in_mongodb(Integer bookingNumber, String name, Integer items) {
+        BookingRequest bookingRequest = bookingRequestRepository.findById(bookingNumber).orElseThrow();
+        assertThat(bookingRequest).isNotNull();
+        assertThat(bookingRequest.getName()).isEqualTo(name);
+        assertThat(bookingRequest.getItems()).isEqualTo(items);
     }
 }
